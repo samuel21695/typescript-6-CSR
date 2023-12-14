65734b76a4765aff66520f342709ea297b3f948a
samuel21695 <samuel21695@gmail.com>
2023-12-14 21:12:53 +0900

    ✨ function fetchexample

--- fetch.ts
+++ fetch.ts
@@ -12,3 +12,18 @@ type Props = {
 // async와 함께 사용한 것을 통해 Promise 객체를 동기적으로 작성된 코드처럼 작동합니다.
 // fetch()는 기본적으로 비동기적으로 작동하기 때문에 비동기 상태로 그대로 두면,
 // 아래의 HTML component 생성하는 부분보다 먼저 실행될 수 있습니다.
+
+async function fetchExample(tagName: string, props: Props, children: string, url: string): Promise<HTMLElement> {
+  // HTML 요소 생성
+  const element =document.createElement(tagName);
+
+  // Props 설정
+  for (const key in props) {
+    element.setAttribute(key, props[key]);
+  }
+
+  // 초기 내용 설정
+  element.innerHTML = children;
+
+  
+}
